c21a9cb7064db41c3c3d673a6588c66b912194e1
Andrew Castellano <castellano.andrew05@gmail.com>
2022-01-31 20:09:35 -0500

    added additional features

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const inp = document.querySelector('#todo-inp');
 const todoContainer = document.querySelector('.todo-container');
 
 const createTodo = (inp) => {
+    // i created the save button here so that i cana reference it in the form event listener
+    const saveBtn = document.createElement('button');
+
     // creates row 
     const row = document.createElement('div');
     row.classList.add('row', 'todo-row', 'mt-4');
@@ -16,8 +19,9 @@ const createTodo = (inp) => {
 
     form.addEventListener('submit', e => {
         e.preventDefault();
-        console.log(form.children[0]);
         form.children[0].setAttribute('disabled', '');
+        saveBtn.classList.add('d-none');
+        editBtn.classList.remove('d-none');
     })
 
     col9.append(form);
@@ -28,18 +32,37 @@ const createTodo = (inp) => {
     buttonCont.classList.add('col-3', 'd-flex', 'justify-content-end');
     row.append(buttonCont);
 
+    // creates edit and save container 
+    const ESContainer = document.createElement('div');
+    ESContainer.classList.add('d-flex', 'align-items-center');
+    buttonCont.append(ESContainer);
+
     // creates edit button
     const editBtn = document.createElement('button');
     editBtn.setAttribute('id', 'edit');
     editBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16"><path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/><path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/></svg>';
+    ESContainer.append(editBtn);
+
+    // adding features to save btn
+    saveBtn.setAttribute('id', 'save');
+    saveBtn.classList.add('text-white', 'd-none');
+    saveBtn.innerHTML = 'SAVE';
+    ESContainer.append(saveBtn);
+
+    saveBtn.addEventListener('click', () => {
+        const inp = editBtn.parentElement.parentElement.parentElement.children[0].children[0].children[0];
+        inp.setAttribute('disabled', '');
+        saveBtn.classList.add('d-none');
+        editBtn.classList.remove('d-none');
+    })
 
     editBtn.addEventListener('click', () => {
-        const inp = editBtn.parentElement.parentElement.children[0].children[0].children[0];
+        const inp = editBtn.parentElement.parentElement.parentElement.children[0].children[0].children[0];
         inp.removeAttribute('disabled');
         inp.focus();
+        editBtn.classList.add('d-none');
+        saveBtn.classList.remove('d-none');
     });
-
-    buttonCont.append(editBtn);
 
     const deleteBtn = document.createElement('button');
     deleteBtn.setAttribute('id', 'delete');
